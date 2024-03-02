8eab1138b4cda848fd34fef2137f3f2003ed3d04
yaswanthnaveen8977 <yvadalas@cbu.edu>
2024-03-02 09:46:24 -0600

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -1,19 +1,32 @@
-""" ex_5_3.py
-This module contains an entry point that:
-
-- creates a CLi that accepts an input file of data to be processed
-- shifts and scales the data to a mean of 0 and a standard deviation 1
-- writes the file to the output file argument
-"""
+# ex_5_3.py
 import numpy as np
 from argparse import ArgumentParser
 
-if __name__ == "__main__":
-    # Create your argument parser object here.
-    # Collect the filename arguments from the command line
-    # Rewrite your 5_3 logic here so that it utilizes the arguments passed from the command line.
+def main(infile, outfile):
+    # Load data from the input file
+    data = np.loadtxt(infile, delimiter=',')
 
-    # Tests will run your command using a system call.
-    # To test your program with arguments, run it from the command line
-    # (see README.md for more details)
-    pass
+    # Modify the input data so that it has a mean of 0
+    zero_mean_data = data - np.mean(data, axis=0)
+
+    # Modify the zero mean data so that it has a standard deviation of 1
+    processed = zero_mean_data / np.std(zero_mean_data, axis=0)
+
+    # Save the processed data to the specified output file
+    np.savetxt(outfile, processed, fmt="%.6f", delimiter=",")
+
+if __name__ == "__main__":
+    # Instantiate an ArgumentParser object
+    parser = ArgumentParser(
+        description="This program applies a standard scale transform to the data in infile and writes it to outfile."
+    )
+
+    # Add positional arguments for input and output files
+    parser.add_argument("infile", help="Input filename for the data file that needs to be processed.")
+    parser.add_argument("outfile", help="Output filename for the processed data.")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the main function with the infile and outfile arguments
+    main(args.infile, args.outfile)
